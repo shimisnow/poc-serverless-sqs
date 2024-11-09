232c233d58490ef9d14bd37b9c1b4e53fb5487cd
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-09 16:01:53 -0300

    build: add webpack config to generate lambda files

---
+++ apps/consumer/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+// Load the TypeScript configuration
+const tsConfig = require('../../tsconfig.json');
+
+// Create a paths mapping
+const alias = {};
+if (tsConfig.compilerOptions.paths) {
+  for (const [key, value] of Object.entries(tsConfig.compilerOptions.paths)) {
+    alias[key.replace('/*', '')] = path.join(
+      path.resolve(__dirname, '../..'), // project root
+      value[0].replace('/*', ''),
+    );
+  }
+}
+
+module.exports = {
+  entry: {
+    main: './apps/consumer/src/main.ts',
+    lambda: './apps/consumer/src/lambda.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, '../../dist/apps/consumer/'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+    clean: true,
+  },
+  resolve: {
+    alias,
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: 'node',
+  externals: [nodeExternals()], // Prevent bundling of node_modules
+  mode: 'development',
+  devtool: 'source-map', // Optional for better debugging
+};
